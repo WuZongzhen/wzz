3f90906eea20df07f62303f60e5172d3ec5c27e2
WuZongzhen <103302166+WuZongzhen@users.noreply.github.com>
2024-07-28 21:12:42 +0800

    Delete dvsgesture directory

--- dvsgesture/train.sh
+++
@@ -1,1 +0,0 @@
-CUDA_VISIBLE_DEVICES=2 python train.py --tb --amp --output-dir ./logs --model PlainNet --device cuda:0 --lr-step-size 64 --epoch 192 --T_train 8 --T 8 --data-path /home/wuzongzhen/wzz-HarDvs/tools/data/HARD/
